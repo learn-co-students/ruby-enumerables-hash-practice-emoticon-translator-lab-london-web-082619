ae893829b0ec62f2adfa1098e3664966485240f7
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-07-01 20:53:22 +0000

    Done.

--- lib/sandbox.rb
+++ lib/sandbox.rb
@@ -1,12 +1,10 @@
-# require modules here
-
 file_path = "./lib/emoticons.yml"
 
-#loads the `emoticons.yml` file and parses the data to the correct format
+#loads the `emoticons.yml` file
 def load_library(file_path)
   require "yaml"
   emoticons = YAML.load_file(file_path)
-  #output of new emoticons hash =>
+  #=>
       # emoticons = {
       #   "angel"=>["O:)", "☜(⌒▽⌒)☞"],
       #   "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
@@ -20,35 +18,15 @@ def load_library(file_path)
     "get_meaning" => {},
     "get_emoticon" => {}
   }
+  #set each Japanese emoticon equal to the key in the get_meaning hash
   emoticons.each do |meaning, emoticons|
-      #set each Japanese emoticon equal to the key in the get_meaning hash; set the meaning to the value
     emoticons_cleaned["get_meaning"]["#{emoticons[1]}"] = meaning
-    #set each English emoticon equal to the key in the get_emoticon has; set the Japanse emoticon to the value
     emoticons_cleaned["get_emoticon"]["#{emoticons[0]}"] = emoticons[1]
   end
-  emoticons_cleaned
+  puts emoticons_cleaned
 end
 
-#takes a traditional Western emoticon, like `:)` and translate it to its Japanese version.
-def get_japanese_emoticon(file_path, emoticon)
-  dictionary = load_library(file_path)
-  if dictionary["get_emoticon"][emoticon]
-    dictionary["get_emoticon"][emoticon]
-  else
-    return "Sorry, that emoticon was not found"
-  end
-end
-
-#takes a Japanese emoticon and returns its meaning in English.
-def get_english_meaning(file_path, emoticon)
-  dictionary = load_library(file_path)
-  if dictionary["get_meaning"][emoticon]
-    dictionary["get_meaning"][emoticon]
-  else
-    return "Sorry, that emoticon was not found"
-  end
-end
-
+load_library(file_path)
 #
 # return = {
 #   "get_meaning" => {
@@ -59,4 +37,12 @@ end
 #     "ENGLISH_EMOTICON_1" =>  "JAPANESE_EMOTICON_1",
 #     "ENGLISH_EMOTICON_2" =>  "JAPANESE_EMOTICON_2",
 #   }
+# }
+#
+# emoticons = {
+#   "angel"=>["O:)", "☜(⌒▽⌒)☞"],
+#   "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
+#   "bored"=>[":O", "(ΘεΘ;)"],
+#   "confused"=>["%)", "(゜.゜)"],
+#   "embarrased"=>[":$", "(#^.^#)"]
 # }
